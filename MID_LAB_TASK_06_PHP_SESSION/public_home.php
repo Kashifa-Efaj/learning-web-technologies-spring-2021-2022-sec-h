2a5c2680561c742824ed972890ffed64cacdfeae
Kashifa-Efaj <88015502+Kashifa-Efaj@users.noreply.github.com>
2022-02-15 22:35:27 +0600

    MID_LAB_TASK_06_PHP_SESSION

---
+++ MID_LAB_TASK_06_PHP_SESSION/public_home.php
@@ -0,0 +1,33 @@
+
+<html >
+<head>
+   
+    <title>Public</title>
+</head>
+
+<body>
+     <form method="POST" action="registration.php">
+          <table border='1'>
+                      <tr>
+                          <b> <th  "hight="2000px", width="1000px" align="left">xCompany <b>
+                            <p align='right'>
+                            <a href='home.php'>Home</a>
+                            <a href='login.php'>Login</a>
+                            <a href='registration.php'>Registration</a>
+                        </p>
+                            
+                            </th>   
+                      </tr>
+
+             <tr>      
+            <td height="100px" width="1000px" align="left"> Welcome to xCompany </td>
+        </tr>
+        <tr>
+       
+            <td  height="30px" width="1000px" align="center"> Copyright 2007</td>
+        </tr>
+
+        </table>
+    </form>
+    </body>
+    </html>
---
+++ MID_LAB_TASK_06_PHP_SESSION/registration.php
@@ -0,0 +1,76 @@
+<html >
+<head>
+   
+    <title>Public</title>
+</head>
+
+<body>
+          <table border='1'>
+                      <tr>
+                          <b> <th  "hight="5000px", width="1000px" align="left">xCompany </b>
+                            <p align='right'>
+                            <a href='home.php'>Home</a>
+                            <a href='login.php'>Login</a>
+                            <a href='registrstion.php'>Registration</a>
+                        </p>
+                            
+                            </th>   
+                      </tr>
+                      <tr >
+                    
+                    <td>
+                        
+
+            <form method="Post" action="registration_check.php">
+    
+            <fieldset>
+          <legend>Registration</legend>
+                      
+        
+          
+          
+          Name: 
+          <input type="text" name="myname" value="" > <br>
+          
+        
+       
+          
+          
+          Email: 
+          <input type="email" name="email" value="" > <br>
+          
+        
+       Users Name:
+          
+         <input type="text" name="myname" value="" > <br>  
+         Password:
+         <input type="password" name=""><br>
+        Confirm  Password:
+        <input type="password" name=""><br>
+        <fieldset>
+         <legend> Gender:</legend>
+        
+            <input type="radio" name="gender" value=""> Male
+            <input type="radio" name="gender" value=""> Female
+            <input type="radio" name="gender" value=""> Other<br>
+            </fieldset>
+            <fieldset>
+          <legend> Date of Birth: </legend> 
+        
+         <input type="date" name="dob" value=""> <br>
+       
+           </fieldset>
+        <input type="submit" name="submit" value="Submit">
+        <input type="reset" name="reset" value="Reset">
+    <tr>
+       
+            <td  height="30px" width="1000px" align="center"> Copyright 2007</td>
+        </tr>
+
+      </fieldset>
+    </form>
+  </td>
+</tr>
+</table>
+</body>
+</html>
