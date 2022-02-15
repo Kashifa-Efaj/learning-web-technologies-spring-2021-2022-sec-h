585e157d25c5681c67cdc6bf039876d3b1274d94
Kashifa-Efaj <88015502+Kashifa-Efaj@users.noreply.github.com>
2022-02-15 13:53:32 +0600

    MID_LAB_TASK_06_PHP_SESSION

---
+++ MID_LAB_TASK_06_PHP_SESSION/login_user.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+	<title>Login user page</title>
+</head>
+
+<body>
+	<table border="1" width="50%">
+		<tr height="50px">
+			<td> Dashboard</td>
+			
+		<th width="150px" rowspan="6" align="center"> Logged in User </th>
+
+			</tr >
+			<tr height="50px">
+			<td>View Profile</td>	
+
+			</tr>
+
+			</tr >
+			<tr height="50px">
+			<td>Edit Profile</td>	
+
+			</tr>
+			</tr >
+			<tr height="50px">
+			<td>Change Profile Picture</td>	
+
+			</tr>
+
+			<tr height="50px">
+			<td>Change Password</td>	
+
+			</tr>
+			<tr height="50px">
+			<td>Logout</td>	
+
+			</tr>
+
+
+
+		</table>
+	</body>
+	</html>
+
+
+
+			
---
+++ MID_LAB_TASK_06_PHP_SESSION/public.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+	<title>public page</title>
+</head>
+
+<body>
+	<table border="1" width="50%">
+		<tr height="50px">
+			<th width="150px" rowspan="4" align="center"> Public User </th>
+			
+			<td  >Public Home</td>
+
+			</tr >
+			<tr height="50px">
+			<td>Registration</td>	
+
+			</tr>
+
+			</tr >
+			<tr height="50px">
+			<td>Login</td>	
+
+			</tr>
+			</tr >
+			<tr height="50px">
+			<td>Forget Password</td>	
+
+			</tr>
+
+		</table>
+	</body>
+	</html>
+
+
+
+			
